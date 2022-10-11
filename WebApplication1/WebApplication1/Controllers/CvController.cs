dc70f9ece9dc179be96a62ee3a8c359ab9668e2c
PritoM-Debnath <94226214+PritoM-Debnath@users.noreply.github.com>
2022-10-12 03:23:31 +0600

    Initial commit

---
+++ WebApplication1/WebApplication1/Controllers/CvController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            dynamic info = new Dictionary<string, string>()
+            {
+                {"Name", "Pritom Debnath"},
+                {"Id", "20-42414-1"},
+                {"Department", "CSE"},
+                {"Email", "debnathpritom@outlook.com"}
+            };
+            ViewBag.Info = info;
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            List<Dictionary<string, string>> projects_list = new List<Dictionary<string, string>>();
+            Dictionary<string, string> projects = new Dictionary<string, string>();
+
+            projects.Add("Name", "Hospital Occupancy");
+            projects.Add("GitLink", "https://github.com/PritoM-Debnath/Web_Tech_Project");
+            projects_list.Add(new Dictionary<string, string>(projects));
+
+            projects["Name"] = "Super Shop Managment System";
+            projects["GitLink"] = "https://github.com/PritoM-Debnath/C-sharp";
+            projects_list.Add(new Dictionary<string, string>(projects));
+
+            ViewBag.projects_list = projects_list;
+
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
