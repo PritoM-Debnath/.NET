26d500af6a6c9fe43fed49a0b52f1339684511db
PritoM-Debnath <94226214+PritoM-Debnath@users.noreply.github.com>
2022-11-04 01:15:46 +0600

    Form Submission

---
+++ FormSubmission/FormSubmission/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class StudentController : Controller
+    {
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+        /*
+        public ActionResult CreateSubmission(FormCollection f)
+        {
+            Student s = new Student();
+            /*
+            s.Id =Request["Id"];
+            s.Name = Request["Name"];
+            s.Cgpa = Request["Cgpa"];
+            //
+
+            s.Id = f["Id"];
+            s.Name = f["Name"];
+            s.Cgpa = f["Cgpa"];
+
+            return View(s);
+        }
+        */
+        public ActionResult CreateSubmission(Student s)
+        {
+            return View(s);
+        }
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public String Cgpa { get; set; }
+    }
+}
